e744e02a75a207e2623c23569defd9285da0dcb2
gjp1120 <gjp1120@gmail.com>
2013-06-06 10:03:28 +0800

    rewrite parse2
    Signed-off-by: gjp1120 <gjp1120@gmail.com>

--- htmlparser/parser2.py
+++ htmlparser/parser2.py
@@ -17,33 +17,48 @@ HTML_HEADER = \
 <body>
 """
 
+def write_out_result(handle, desc, img, links):
+  # Write Out
+  # Desc
+  handle.write("<p>\n")
+  handle.write(describe)
+  handle.write("</p>\n")
+  # Links
+  handle.write("<p>\n")
+  for link in links:
+    handle.write("%s<br/>\n" % link)
+  handle.write("</p>\n")
+  # Image
+  handle.write("<img src=\"%s\"/>\n" % img[1:])
+
 def init_file_handle(fh):
   fh.write(HTML_HEADER)
 
 for filename in sys.argv[1:]:
+  # FILE1
   with open(filename) as fd:
     etr1 = lxml.etree.HTML(fd.read().decode("gb18030"))
+    desc = "".join([s.encode("utf8") for s in etr1.xpath("//div[@class='list']").pop().itertext()])
+    img  = etr1.xpath("//div[@class='list']/table/tr[1]/td/img/@src").pop()
+    content_type    = etr1.xpath("//div[@class='list']/table/tr[1]/td[1]/text()").pop()[5:]
+    player_filename = etr1.xpath("//ul/li/a/@href").pop()
 
-  describe = "".join([s.encode("utf8") for s in etr1.xpath("//div[@class='list']").pop().itertext()])
-  img = etr1.xpath("//div[@class='list']/table/tr[1]/td/img/@src").pop()
-
-  content_type = etr1.xpath("//div[@class='list']/table/tr[1]/td[1]/text()").pop()[5:]
+  # Create Type
   if content_type not in FILEH:
     FILEH[content_type] = open("%s.html" % content_type, "w")
     init_file_handle(FILEH[content_type])
-  handle = FILEH[content_type]
 
-  player_filename = etr1.xpath("//ul/li/a/@href").pop()
-
+  # FILE2
   with open(os.path.join(PREFIX, player_filename[1:])) as fd:
     etr_player = lxml.etree.HTML(fd.read().decode("gb18030"))
+    player_data_filename = etr_player.xpath("//td[@align='center']/script/@src").pop()
 
-  player_data_filename = etr_player.xpath("//td[@align='center']/script/@src").pop()
-
+  # FILE3
   with open(os.path.join(PREFIX, player_data_filename[1:])) as fd:
     s = re.findall("\[\[.*\]\]\]", fd.read()).pop()
     s = s.replace("'", '"').decode("gb18030")
 
+  # Parse Json
   try:
     arry = json.loads(s)[0][1]
     links = [l.encode('utf8') for l in arry]
@@ -52,17 +67,10 @@ for filename in sys.argv[1:]:
     print >>sys.stderr, e
     links = []
 
-  handle.write("<p>\n")
-  handle.write(describe)
-  handle.write("</p>\n")
+  # Write Out
+  write_out_result(FILEH[content_type], desc, img, links)
 
-  handle.write("<p>\n")
-  for link in links:
-    handle.write("%s<br/>\n" % link)
-  handle.write("</p>\n")
-
-  handle.write("<img src=\"%s\"/>\n" % img[1:])
-
+# Parse End
 for key in FILEH:
   FILEH[key].write("</body>\n</html>\n")
   FILEH[key].close()
