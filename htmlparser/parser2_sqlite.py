e87c036e177148547edb4235b65fd999ac9db906
gjp1120 <gjp1120@gmail.com>
2013-06-07 22:34:31 +0800

    now parser2_sqlite can parse the table(@class=list) fully

--- htmlparser/parser2_sqlite.py
+++ htmlparser/parser2_sqlite.py
@@ -14,21 +14,26 @@ TABLES = {}
 handle = sqlite3.connect("database.db")
 cursor = handle.cursor()
 
-def write_out_result(table, desc, img, links):
-  cursor.execute("insert into %s values (?, ?, ?)" % table, (desc.decode("utf8"), img.decode("utf8"), "|".join(links).decode("utf8")))
+def write_out_result(table, title, area, date, img, links):
+  cursor.execute("insert into %s values (?, ?, ?, ?, ?)" % table, (title, area,
+                                                                   date, img, ",".join(links)))
 
 for filename in sys.argv[1:]:
   # FILE1
   with open(filename) as fd:
     etr1 = lxml.etree.HTML(fd.read().decode("gb18030"))
-    desc = "".join([s.encode("utf8") for s in etr1.xpath("//div[@class='list']").pop().itertext()])
-    img  = etr1.xpath("//div[@class='list']/table/tr[1]/td/img/@src").pop()
-    content_type    = etr1.xpath("//div[@class='list']/table/tr[1]/td[1]/text()").pop()[5:]
+    #desc = "".join([s.encode("utf8") for s in etr1.xpath("//div[@class='list']").pop().itertext()])
+    table = etr1.xpath("//div[@class='list']/table").pop()
+    img   = table.xpath("tr[1]/td/img/@src").pop()
+    content_type = table.xpath("tr[1]/td[1]/text()").pop()[5:]
+    name  = table.xpath("tr[2]/td/strong/text()").pop()
+    area  = table.xpath("tr[3]/td/text()").pop()[5:]
+    date  = table.xpath("tr[4]/td/text()").pop()
     player_filename = etr1.xpath("//ul/li/a/@href").pop()
 
   # Create Type
   if content_type not in TABLES:
-    handle.execute("create table %s (desc text, img text, links text)" % content_type)
+    handle.execute("create table %s (TITLE text, AREA text, DATE text, IMG text, LINKS text)" % content_type)
     TABLES[content_type] = 1
 
   # FILE2
@@ -43,15 +48,14 @@ for filename in sys.argv[1:]:
 
   # Parse Json
   try:
-    arry = json.loads(s)[0][1]
-    links = [l.encode('utf8') for l in arry]
+    links = json.loads(s)[0][1]
   except UnicodeDecodeError as e:
     sys.stderr.write("error! parse %s failed\n" % player_data_filename)
     print >>sys.stderr, e
     links = []
 
   # Write Out
-  write_out_result(content_type, desc, img, links)
+  write_out_result(content_type, name, area, date, img, links)
 
 cursor.close()
 handle.commit()
